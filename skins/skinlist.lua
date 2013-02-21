caa7e38c725bdb4f9a4da39b7a01fec503d27f36
0gb.us <0gb.us@0gb.us>
2013-02-21 00:28:54 -0800

    Modified code to only save data when data is actually modified, removed redundant images, and indexed skins from zero.

--- skins/skinlist.lua
+++ skins/skinlist.lua
@@ -3,18 +3,18 @@
 -- Released by Zeg9 under WTFPL
 -- Have fun !
 
-skins.list = {}
+skins.list = {"player","character"}
 skins.add = function(skin)
 	table.insert(skins.list,skin)
 end
 
-id = 1
+id = 0
 while io.open(minetest.get_modpath("skins").."/textures/player_"..id..".png") do
 	skins.add("player_"..id)
 	id = id +1
 end
 
-id = 1
+id = 0
 while io.open(minetest.get_modpath("skins").."/textures/character_"..id..".png") do
 	skins.add("character_"..id)
 	id = id +1
