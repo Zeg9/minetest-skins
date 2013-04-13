e6df1f47acab7eea4ba58d8160c58f5275aaf52b
Zeg9 <dazeg9@gmail.com>
2013-04-13 17:20:36 +0200

    Update original skin, give previews a transparent background and update generate_previews.sh

--- generate_previews.sh
+++ generate_previews.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd .previews
+rm ../skins/textures/*_preview.png # Remove all previous previews
 for i in ../skins/textures/character_*.png;
 do
 	cp $i ./character.png
 	blender --background character.blend \
 		--render-output '//character_##.png' \
-		--render-frame 0
-	mv character_00.png $i"_preview.png"
+		--render-frame 0 # Render the preview thanks to blender
+	mv character_00.png $i"_preview.png" # put this in the textures folder
 	echo "Generated preview for $i"
 done
 echo "Generated previews !"
