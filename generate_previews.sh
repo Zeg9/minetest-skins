fc348be32028b7c7e1231b7caf281ff1a9e5cdf8
Zeg9 <dazeg9@gmail.com>
2013-06-29 16:24:46 +0200

    Strip metadata from previews (will allow updating 3d skins without re-uploading all previews)

--- generate_previews.sh
+++ generate_previews.sh
@@ -2,7 +2,10 @@
 # This script is used to generate the previews needed by the mod
 # It requires blender (2.6x is tested, but others versions might work)
 # ...as well as pngcrush to reduce the size. If you don't want to use pngcrush, set PNGCRUSH=false below
+# ...also, you will need imagemagick in order to remove the metadata from blender's output (allows to only upload the different files to git)
+#    same as for pngcrush, there is a setting below to disable it
 PNGCRUSH=true
+IMAGEMAGICK=true
 cd .previews
 rm ../skins/textures/*_preview*.png # Remove all previous previews
 mkdir -p output
@@ -10,13 +13,19 @@ for i in ../skins/textures/character_*.png;
 do
 	cp $i ./character.png
 	blender --background character.blend \
-		--render-output '//character_##.png' -a
+		--render-output "//character_##.png" -a
 	out_name=$(basename $i)
 	out_name="${out_name%.*}"
 	out_file=output/"$out_name"_preview.png
 	back_out_file=output/"$out_name"_preview_back.png
-	mv character_00.png $out_file
-	mv character_01.png $back_out_file
+	if $IMAGEMAGICK
+	then
+		convert -strip character_00.png $out_file
+		convert -strip character_01.png $back_out_file
+	else
+		mv character_00.png $out_file
+		mv character_01.png $back_out_file
+	fi
 	echo "Generated preview for $i"
 done
 if $PNGCRUSH
