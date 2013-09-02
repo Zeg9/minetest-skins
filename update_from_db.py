6888c09cb021ab91a9215f9dd7196bfb7b2e356e
Zeg9 <dazeg9@gmail.com>
2013-09-02 18:46:32 +0200

    This actually works! Thanks addi!

--- update_from_db.py
+++ update_from_db.py
@@ -6,28 +6,36 @@ import base64
 server = "minetest.fensta.bplaced.net"
 skinsdir = "skins/textures/"
 metadir = "skins/meta/"
+i = 1
+pages = 0
 
 c = HTTPConnection(server)
-c.request("GET","/api/get.json.php?getlist&page=1&outformat=base64")
-r = c.getresponse()
-if r.status != 200:
-	print("Error", r.status)
-	exit(r.status)
-data = r.read().decode()
-l = json.loads(data)
-if not l["success"]:
-	print("Success != True")
-	exit(1)
-i = 1
-for s in l["skins"]:
-	f = open(skinsdir+"character_"+str(i)+".png",'wb')
-	f.write(base64.b64decode(s["img"]))
-	f.close()
-	f = open(metadir+"character_"+str(i)+".txt",'w')
-	f.write('name = "'+s["name"]+'",\n')
-	f.write('author = "'+s["author"]+'",\n')
-	f.write('comment = "'+s["license"]+'",\n')
-	f.close()
-	i = i + 1
+def addpage(page):
+	global i, pages
+	c.request("GET","/api/get.json.php?getlist&page="+str(page)+"&outformat=base64")
+	r = c.getresponse()
+	if r.status != 200:
+		print("Error", r.status)
+		exit(r.status)
+	data = r.read().decode()
+	l = json.loads(data)
+	if not l["success"]:
+		print("Success != True")
+		exit(1)
+	pages = int(l["pages"])
+	for s in l["skins"]:
+		f = open(skinsdir+"character_"+str(i)+".png",'wb')
+		f.write(base64.b64decode(s["img"]))
+		f.close()
+		f = open(metadir+"character_"+str(i)+".txt",'w')
+		f.write('name = "'+s["name"]+'",\n')
+		f.write('author = "'+s["author"]+'",\n')
+		f.write('comment = "'+s["license"]+'",\n')
+		f.close()
+		i = i + 1
+addpage(1)
+if pages > 1:
+	for p in range(pages-1):
+		addpage(p+2)
 print("Skins have been updated. Please run ./generate_previews.sh")
 
